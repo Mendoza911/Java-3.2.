c731d378f9b2c9ada405c63eacae0cdb7e91d5e2
Kirill Markov <isikir668@gmail.com>
2023-04-09 10:00:36 +0300

    Задание 2. rework 2.

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // TaskTas 2.
         int balance = 100;
-        int replenishment = 3000;
-        int bonus = (balance + replenishment - 1000) / 100;
-        int result = balance + replenishment;
-        int resultb = balance + replenishment + bonus;
+        int replenishment = 1100;
+        int bonus = replenishment / 100;
+        int total_without_bonuses = balance + replenishment;
+        int total_with_bonuses = balance + replenishment + bonus;
 
         if (replenishment < 1000) {
-            System.out.print("Бонусов нет, итоговая сумма на счету клиента - " + result);
+            System.out.print("Бонусов нет, итоговая сумма на счету клиента - " + total_without_bonuses);
         } else {
-            System.out.print("Бонус равен - " + bonus + " рублям, итоговая сумма на счету клиента — " + resultb);
+            System.out.print("Бонус равен - " + bonus + " рублям, итоговая сумма на счету клиента — " + total_with_bonuses + " рублей.");
         }
     }
 }
